3e20a0ad25f4ecc3b99550b786b1bb9c019a1273
Daniel Ruhman <daniruhman@Daniels-MacBook-Pro.local>
2017-03-20 16:18:57 -0300

    or gate

--- src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/OrGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/OrGate.java
@@ -1,22 +1,33 @@
 package br.pro.hashi.ensino.desagil.lucianogic.model;
 
-public class OrGate extends Gate{
-	// Não fiz ainda
+public class OrGate extends Gate {
 	private NandGate nandGate;
+	private NandGate nandGate1;
+	private NandGate nandGate2;
 
 	public OrGate() {
 		super(2);
 		nandGate = new NandGate();
+		nandGate1 = new NandGate();
+		nandGate2 = new NandGate();
 	}
 
 	@Override
 	public boolean read() {
-		return nandGate.read();
+		return nandGate2.read();
 	}
 
 	@Override
 	protected void doConnect(Emitter emitter, int index) {
-		nandGate.connect(emitter, 0);
-		nandGate.connect(emitter, 1);
+		if(index==0){
+			nandGate.connect(emitter, 0);
+			nandGate.connect(emitter, 1);
+		}
+		if(index==1){
+			nandGate1.connect(emitter, 0);
+			nandGate1.connect(emitter, 1);
+			nandGate2.connect(nandGate, 0);
+			nandGate2.connect(nandGate1, 1);			
+		}
 	}
 }
