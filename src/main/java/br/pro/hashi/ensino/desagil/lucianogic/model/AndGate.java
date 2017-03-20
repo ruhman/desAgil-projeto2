0e40ec498a836851183b4dc8c845bd3d7b6c5cfd
Marcelo Prado <marceloterreiroprado@gmail.com>
2017-03-20 16:08:35 -0300

    And working

--- src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/AndGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/AndGate.java
@@ -1,31 +1,34 @@
 package br.pro.hashi.ensino.desagil.lucianogic.model;
 
 public class AndGate extends Gate{
-	private NandGate nandGate;
-	private OrGate orGate;
+	private NandGate nandGate1;
+	private NandGate nandGate2;
 
 	public AndGate() {
 		super(2);
-		nandGate = new NandGate();
-		orGate = new OrGate();
+		nandGate1 = new NandGate();
+		nandGate2 = new NandGate();
 	}
 
 	@Override
 	public boolean read() {
-		// Quero ler apenas a saída do Or
-		return nandGate.read() && orGate.read();
+		return nandGate2.read();
 	}
 
 	@Override
 	protected void doConnect(Emitter emitter, int index) {
 		
-		// Queria conectar o nand na entrada do or, mas não entendi como faz isso
+		// Queria conectar a saida do nand1 na entrada do nand2, mas não entendi como faz isso		
+		if (index == 0) {
+			nandGate1.connect(emitter, 0);
+		} else {
+			nandGate1.connect(emitter, 1);
+		}
+		nandGate2.connect(nandGate1, 0);
+		nandGate2.connect(nandGate1, 1);
 		
-		nandGate.connect(emitter, 0);
-		nandGate.connect(emitter, 1);
-		
-		orGate.connect(emitter, 0);
-		orGate.connect(emitter, 1);
+	
+
 	}
 
 }
