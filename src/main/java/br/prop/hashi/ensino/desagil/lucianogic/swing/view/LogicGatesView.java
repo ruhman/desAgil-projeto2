192fec272c029bfa003d8495b00387204dba8ce7
Marcelo Prado <marceloterreiroprado@gmail.com>
2017-03-28 07:03:27 -0300

    Created UI for logic gate, working on gate input logic

---
+++ src/main/java/br/prop/hashi/ensino/desagil/lucianogic/swing/view/LogicGatesView.java
@@ -0,0 +1,89 @@
+package br.prop.hashi.ensino.desagil.lucianogic.swing.view;
+
+import java.awt.event.ItemEvent;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.awt.event.ItemListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import br.pro.hashi.ensino.desagil.lucianogic.model.Gate;
+
+
+public class LogicGatesView extends JPanel implements ItemListener {
+
+	// Necessario para serializar objetos desta classe.
+	private static final long serialVersionUID = 1L;
+	
+	private Gate gate;
+	private LinkedList<JCheckBox> checkBoxes;
+	private JCheckBox resultCheckbox;
+	
+	public LogicGatesView(Gate gate) {
+		this.gate = gate;
+		
+		// A componente JLabel representa simplesmente um texto fixo.
+		JLabel inputField = new JLabel("Input");
+		JLabel outputField = new JLabel("Output");
+
+		this.gate = gate;
+			
+		// set checkbox here
+		resultCheckbox.setEnabled(false);
+
+		// stack components at vertical layout
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		// Create the correct amount of checkboxes
+		this.checkBoxes = new LinkedList<>();
+		for (int i = 0; i < gate.getSize(); i++) {
+			JCheckBox checkbox = new JCheckBox(""+i);
+			checkbox.addItemListener(this);
+			this.checkBoxes.add(checkbox);
+		}
+		
+		add(inputField);
+		
+		// Adds all checkboxes to view
+		for (JCheckBox checkBox : this.checkBoxes) {
+			add(checkBox);
+		}
+		
+		add(outputField);
+		add(resultCheckbox);
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+	    Object source = e.getItemSelectable();
+	    
+	    
+	    // Proximos passos:
+	    // 1. Inicializar a porta logica com todos os pinos setados para 0 no construtor
+	    // 2. Usar a classe switch para modificar os pinos
+	    // 3. Settar a checkbox de resultado para o valor de output da porta
+	    
+	    if (source == checkBoxes.get(0)) {
+	    	System.out.println("First");
+	    	
+	    } else if (source == checkBoxes.get(1)) {
+	    	System.out.println("second");
+	    } else  if (source == checkBoxes.get(2)) {
+	    	System.out.println("third");
+	    	
+	    }
+
+	    if (e.getStateChange() == ItemEvent.DESELECTED) {
+	    	System.out.println("Deselected");
+	    }
+	    
+	    
+	    
+	    
+	}
+
+}
---
+++ src/main/java/br/prop/hashi/ensino/desagil/lucianogic/swing/view/MainView.java
@@ -0,0 +1,64 @@
+package br.prop.hashi.ensino.desagil.lucianogic.swing.view;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import br.pro.hashi.ensino.desagil.lucianogic.model.Gate;
+
+public class MainView extends JPanel implements ActionListener {
+
+	// Necessario para serializar objetos desta classe.
+	private static final long serialVersionUID = 1L;
+
+
+	// A componente JComboBox representa um menu.
+	// https://docs.oracle.com/javase/tutorial/uiswing/components/combobox.html
+	private JComboBox<Gate> comboBox;
+
+	private LogicGatesView logicGatesView;
+
+
+	public MainView(List<Gate> model) {
+		comboBox = new JComboBox<>();
+
+		for(Gate gate: model) {
+			comboBox.addItem(gate);
+		}
+
+		comboBox.addActionListener(this);
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		add(comboBox);
+
+		addLogicGateView(comboBox.getItemAt(0));
+	}
+
+	private void addLogicGateView(Gate gate) {
+		logicGatesView = new LogicGatesView(gate);
+		add(logicGatesView);
+	}
+
+
+	/* A PARTIR DESTE PONTO VOCE DEVE ENTENDER SOZINHO */
+
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		remove(logicGatesView);
+
+		int index = comboBox.getSelectedIndex();
+		Gate gate = comboBox.getItemAt(index);
+		addLogicGateView(gate);
+
+		// Necessario para atualizar a janela.
+		JFrame frame = (JFrame) SwingUtilities.getRoot(this);
+		frame.pack();
+	}
+}
